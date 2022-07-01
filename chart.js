6abdc4b2c3ad4e8fdf2ffb592cc96c5e25e4b3d5
tayyebeh <tbadiee73@gmail.com>
2022-07-01 20:31:27 +0430

    add header details in chart page

--- chart.js
+++ chart.js
@@ -1,11 +1,45 @@
 let params =new URLSearchParams(document.location.search);
 let coin_name = params.get("coin");
 
-function coin_url(name) {
-    return `https://api.coincap.io/v2/assets/${name}/history?interval=d1`;
+
+//header
+
+function coin_item (name){
+  return `https://api.coincap.io/v2/assets/${name}`;
+}
+
+async function get_coin_item() {
+  let items=coin_item(coin_name);
+  let response=await fetch(res);
+  let json=await response.json();
+
+  let data=json.data;
+  return data;
+    
 }
 
 
+
+let coin_header =document.querySelector(".section_coin_header_items");
+
+function render_coin_item(items) {
+
+
+
+
+
+
+}
+
+
+
+
+//chart
+
+
+function coin_url(name) {
+    return `https://api.coincap.io/v2/assets/${name}/history?interval=d1`;
+}
 
 async function get_coin() {
     
